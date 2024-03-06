db8c2ef2273f802dc184d0ed8e70c06f95a5c62f
mythofsummer <joungeun0825@naver.com>
2024-03-06 23:51:44 +0900

    데이터 미리 넣어놓음, 책갈피 상세페이지

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO book (title, author, publisher, created_at, updated_at) VALUES ('제목1', '작가1', '출판사1', NOW(), NOW())
+INSERT INTO book (title, author, publisher, created_at, updated_at) VALUES ('제목2', '작가2', '출판사2', NOW(), NOW())
+INSERT INTO book (title, author, publisher, created_at, updated_at) VALUES ('제목3', '작가3', '출판사3', NOW(), NOW())
+
+INSERT INTO quote (content, page, book_id, created_at, updated_at) VALUES ('content입니다', 32, 1, NOW(), NOW())
+INSERT INTO quote (content, page, book_id, created_at, updated_at) VALUES ('content2입니다', 33, 1, NOW(), NOW())
+INSERT INTO quote (content, page, book_id, created_at, updated_at) VALUES ('content입니다', 32, 2, NOW(), NOW())
+INSERT INTO quote (content, page, book_id, created_at, updated_at) VALUES ('content2입니다', 32, 2, NOW(), NOW())
